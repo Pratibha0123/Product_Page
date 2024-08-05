06f7f39a213bd95b352b75753f5f802eb18b3b51
Pratibha Sharma <137270910+Pratibha0123@users.noreply.github.com>
2024-08-05 23:16:52 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  Router, Route, Routes, HashRouter } from 'react-router-dom';
 import Product_page from './Components/Product_Page/Product_page';
 // import YourSizeModal from "./Components/YourSizeModal/YourSizeModal"
 
@@ -10,12 +10,12 @@ import EditProfilePage from './Components/EditProfilePage/EditProfilePage';
 const App = () => {
   return (
    <>
-   <Router>
+   <HashRouter>
       <Routes>
         <Route path="/edit-profile" element={<EditProfilePage />} />
         <Route path="/" element={<Product_page />} />
       </Routes>
-    </Router>
+    </HashRouter>
    </>
   )
 }
